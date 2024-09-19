4e523d5fdcc0c4455a59a81b020f7910dca2a322
Alexey Petrov <alexeypetrov@Alexeys-MacBook-Pro.local>
2024-09-19 11:41:36 +0300

    route middleware

---
+++ src/app/dashboard/features/Header/api/route.ts
@@ -0,0 +1,17 @@
+import { useSSRFetch } from "@/app/dashboard/utils/serverSideFetcher";
+import { cookies } from "next/headers";
+
+export async function POST() {
+  const res = await useSSRFetch().protectedFetcher("logout", {
+    method: "POST",
+    headers: {
+      Cookie: `access_token=${
+        cookies().get("access_token")?.value
+      };refresh_jti=${cookies().get("refresh_jti")?.value}`,
+    },
+  });
+
+  const data = await res.json();
+
+  return Response.json({ data });
+}
