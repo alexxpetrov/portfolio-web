25f57c901ed97fefed9e917917c086f6bbad0821
Alexey Petrov <alexeypetrov@Alexeys-MacBook-Pro.local>
2024-09-30 17:45:20 +0300

    config fix

--- app/dashboard/utils/config.ts
+++ app/dashboard/utils/config.ts
@@ -2,6 +2,6 @@ import "../../../envConfig.ts";
 
 export const ENDPOINT = process.env.NEXT_PUBLIC_HOST || "http://localhost:4000";
 export const WEBAUTHN_ENDPOINT =
-  process.env.NEXT_PUBLIC_WEBAUTHN || "http://localhost:5000";
+  process.env.NEXT_PUBLIC_WEBAUTHN_ENDPOINT || "http://localhost:5000";
 export const ENVIRONMENT = process.env.NEXT_PUBLIC_ENV || "development";
 export const IS_DEVELOPMENT = ENVIRONMENT === "development";
